2d4f173975c8cdd3855773761c6134b6f0d30b5a
“harrysokool” <harry1232003@icloud.com>
2023-06-08 21:10:32 +0800

    added button color

--- behavior.js
+++ behavior.js
@@ -1,24 +1,18 @@
-const number_button = document.querySelectorAll('[data_number]');
-const op_button = document.querySelectorAll('[data_op]');
-const eq_button = document.querySelector('[data_eq]');
-const clear_button = document.querySelector('[data_clear]');
-const prev_op = document.querySelector('[prev_operand]');
-const curr_op = document.querySelector('[curr_operand]');
+class Calculator {
+  constructor(prev_op, curr_op) {
+    this.prev_op = prev_op;
+
+  }
+}
 
 function updateResult() {
-  let result = document.getElementById("result");
 }; 
 
-updateResult();
 
 function updateInput() {
-  let input = document.getElementById("result");
-  input.setAttribute("value", 7);
 };
 
 function clearInput() {
-  let input = document.getElementById("result");
-  input.setAttribute("value", 0);
 };
 
 
