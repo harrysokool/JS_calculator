84982defbd8f9524511463aae01077c0e511fdf8
“harrysokool” <harry1232003@icloud.com>
2023-06-08 20:51:16 +0800

    changed output from input

--- behavior.js
+++ behavior.js
@@ -1,6 +1,12 @@
+const number_button = document.querySelectorAll('[data_number]');
+const op_button = document.querySelectorAll('[data_op]');
+const eq_button = document.querySelector('[data_eq]');
+const clear_button = document.querySelector('[data_clear]');
+const prev_op = document.querySelector('[prev_operand]');
+const curr_op = document.querySelector('[curr_operand]');
+
 function updateResult() {
   let result = document.getElementById("result");
-  // result.value = "hello";
 }; 
 
 updateResult();
@@ -14,5 +20,6 @@ function clearInput() {
   let input = document.getElementById("result");
   input.setAttribute("value", 0);
 };
+
 
 
