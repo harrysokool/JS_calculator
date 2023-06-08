c997a35900450867c9199d3c975eee22f2e8fc45
“harrysokool” <harry1232003@icloud.com>
2023-06-08 18:44:02 +0800

    testing button with js file

--- behavior.js
+++ behavior.js
@@ -1,6 +1,13 @@
 function updateResult() {
   let result = document.getElementById("result");
-  result.value = "hello";
+  // result.value = "hello";
 }; 
 
 updateResult();
+
+function updateInput() {
+  let input = document.getElementById("result");
+  input.setAttribute("value", 7);
+};
+
+
