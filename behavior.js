09974d8396ef887ffe76a34827d5321978771d11
“harrysokool” <harry1232003@icloud.com>
2023-06-08 21:35:50 +0800

    set up for the functions for calc.

--- behavior.js
+++ behavior.js
@@ -1,19 +1,39 @@
+const number_button = document.querySelectorAll('[data_number]');
+const op_button = document.querySelectorAll('[data_op]');
+const eq_button = document.querySelector('[data_eq]');
+const clear_button = document.querySelector('[data_clear]');
+const prev_op = document.querySelector('[prev_operand]');
+const curr_op = document.querySelector('[curr_operand]');
+
 class Calculator {
   constructor(prev_op, curr_op) {
     this.prev_op = prev_op;
-
   }
 }
 
-function updateResult() {
-}; 
-
-
-function updateInput() {
+// clear everything in the output box
+function clear() {
 };
 
-function clearInput() {
+// add input
+function append(number) {
+
 };
+
+// choose operation
+function operation(operation) {
+  // can use eval 
+};
+
+function compute() {
+  
+};
+
+function updateOutput() {
+
+};
+
+
 
 
 
